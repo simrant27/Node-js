fc2a00b267c3a099bf2dc3d5babe791e20b82595
Simran Tamang <simrant009@gmail.com>
2024-09-17 13:09:09 +0545

    update user

--- node_express/routes/api/users.js
+++ node_express/routes/api/users.js
@@ -36,4 +36,18 @@ router.post("/", (req, res) => {
   res.json(users);
 });
 
+//update
+router.put("/:id", (req, res) => {
+  const found = users.some((user) => user.id === parseInt(req.params.id));
+  if (found) {
+    const updateUser = req.body;
+    users.forEach((user) => {
+      if (user.id === parseInt(req.params.id)) {
+        user.name = updateUser.name ? updateUser.name : user.name;
+        user.email = updateUser.email ? updateUser.email : user.email;
+        res.json({ msg: "user updated", user });
+      }
+    });
+  }
+});
 module.exports = router;

17d544c022dd87f15854023841e1cf3cfccd3aa6
Simran Tamang <simrant009@gmail.com>
2024-09-17 19:39:51 +0545

    select update and delete query

--- simplilearn/node_mysql/index.js
+++ simplilearn/node_mysql/index.js
@@ -54,6 +54,43 @@ app.get("/employee1", (req, res) => {
   });
 });
 
+//select employee
+app.get("/getemployee", (req, res) => {
+  let sql = "SELECT * FROM EMPLOYEE";
+  let query = db.query(sql, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    console.log(results);
+    res.send("employee details fetched");
+  });
+});
+
+//update employee
+app.get("/updateemployee/:id", (req, res) => {
+  let newUser = "Update name";
+  let sql = `UPDATE employee SET name = '${newUser}' WHERE id = ${req.params.id}`;
+
+  let query = db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+
+    res.send("employee updated");
+  });
+});
+
+//delete employee
+app.get("/deleteemployee/:id", (req, res) => {
+  let sql = `DELETE FROM employee WHERE id= ${req.params.id}`;
+  db.query(sql, (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send("Deleted successfully");
+  });
+});
+
 app.listen(3000, (req, res) => {
   console.log("server started on port 3000");
 });

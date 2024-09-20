422528b0e9603c18e0c3ea0e35eab46657226161
Simran Tamang <simrant009@gmail.com>
2024-09-20 09:57:28 +0545

    wrapper module in new folder

---
+++ node_and_mongod/ch2/index.js
@@ -0,0 +1,9 @@
+// this code is wrapped in wrapper function by node
+//module wrapper
+// (function (exports, require, module, __filename, __dirname) {
+//   console.log("Helloo");
+// });
+console.log("Hello world");
+console.log(__dirname);
+console.log(__filename);
+// console.log(module);
